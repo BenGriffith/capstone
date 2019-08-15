2faee886f24e07f91c8f016d96cc5419ced3f54a
Ben Griffith <bengriffith@outlook.com>
2019-08-14 21:07:37 -0500

    update to convert_age_to_days function

--- util.py
+++ util.py
@@ -1,5 +1,21 @@
 
-# convert age
-def get_age(age):
-    pass
+def convert_age_to_days(age):
+    '''
+    Converting value from AgeuponOutcome to age in number of days
+    '''
 
+    days_in_week = 7
+    days_in_month = 30
+    days_in_year = 365
+    age_list = age.split()
+
+    if age_list[1][0] == 'w':
+        age_in_days = int(age_list[0]) * 7
+    elif age_list[1][0] == 'm':
+        age_in_days = int(age_list[0]) * 30
+    elif age_list[1][0] == 'y':
+        age_in_days = int(age_list[0]) * 365
+    else:
+        age_in_days = int(age_list[0])
+
+    return age_in_days

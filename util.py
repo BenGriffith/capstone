302e62ada19ffee79ac937b6e9b09d2129a46601
Ben Griffith <bengriffith@outlook.com>
2019-08-19 21:47:34 -0500

    add getBreed and getColor functions

--- util.py
+++ util.py
@@ -18,3 +18,42 @@ def convertAgeToDays(age):
         age_in_days = int(age_list[0])
 
     return age_in_days
+
+def getBreed(animal_breed):
+    '''
+    getBreed takes in one argument and if 'Mix' or '/' is found these values will be removed or replaced with whitespace
+    '''
+    breed_name = ''
+    breed = animal_breed.split()
+    slash_exist = False
+
+    if 'Mix' in breed:
+        breed.pop()
+        breed_name = ' '.join(breed)
+    else:
+        breed = ' '.join(breed)
+        for i in breed:
+            if i == '/' :
+                slash_exist = True
+        
+        if slash_exist:
+            breed_name = breed.replace('/', ' ')
+        else:
+            breed_name = breed
+    
+    return breed_name
+
+def getColor(animal_color):
+    '''
+    getColor takes in animal_color and returns one of two values based on whether a '/' is found
+    in the argument
+    '''
+
+    for i in animal_color:
+        if i == '/':
+            color = 'Mixed'
+            break
+        else:
+            color = 'Non-Mixed'
+    
+    return color
